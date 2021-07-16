5cdb42c81675615fe51c0a89980ad0f14630509a
Engin Diri <engin.diri@mail.schwarz>
2021-07-16 21:56:12 +0200

    add .bin/ to golangci-lint.sh
    Signed-off-by: Engin Diri <engin.diri@mail.schwarz>

--- pre_commit_hooks/go/golangci-lint.sh
+++ pre_commit_hooks/go/golangci-lint.sh
@@ -4,6 +4,8 @@ set -e
 
 if hash golangci-lint 2>/dev/null; then
   golangci-lint run --timeout 10m -E goimports --fix "$@"
+elif hash ./bin/golangci-lint 2>/dev/null; then
+  ./bin/golangci-lint run --timeout 10m -E goimports --fix "$@"
 else
   echo "golangci-lint not installed. Skipping golangci-lint." && exit 1
 fi
